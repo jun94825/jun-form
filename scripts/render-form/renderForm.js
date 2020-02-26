c391349e7e90923c17f171edf315ba983bcb187b
Jun <jun@advmeds.com>
2020-02-26 09:30:42 +0800

    不用 cdn 載 Bootstrap，單獨載入 grid

--- scripts/render-form/renderForm.js
+++ scripts/render-form/renderForm.js
@@ -7,30 +7,34 @@ import './components/number.js';
 import './components/email.js';
 import './components/english.js';
 
+// https://github.com/mengxiong10/vue2-datepicker/blob/master/README.zh-CN.md
+
 export default Vue.component('RenderForm', {
-  data() {
-    return {
-      form: {},
-      totalScore: 0,
-      readOnlyMode: false,
-      // 不重要
-      previewMode: false,
-    };
-  },
+  data: () => ({
+    form: {},
+    totalScore: 0,
+    readOnlyMode: false,
+    // Unimportant
+    previewMode: false,
+  }),
   template: `
     <div>
       <div class="container">
-        <div class="row no-gutters">
+        <div class="row">
           <div class="col">
 
-            <h4 v-if="totalScore !== 0" class="text-right">得分：<span class="badge badge-info">{{ totalScore }}</span></h4>
+            <h4 v-if="totalScore !== 0">得分：
+              <span>{{ totalScore }}</span>
+            </h4>
             
-            <div class="t">
+            <div>
               <p>{{ form.Title }}</p>
             </div>
-            <div class="d">
+
+            <div>
               <p>{{ form.Description }}</p>
             </div>
+
             <component
               v-for="(item, index) in form.Questions"
               :key="index"
@@ -42,6 +46,7 @@ export default Vue.component('RenderForm', {
               @show="showScore"
               :pMode="previewMode"
             ></component>
+
           </div>
         </div>
       </div>
@@ -135,9 +140,9 @@ export default Vue.component('RenderForm', {
       this.form = obj;
     },
     checkRequired() {
-      // 除了確認該題目是否為必填而且答案是否為空值之外，
-      // 如果題目是必填但沒有出現在畫面上則忽略該題，
-      // 沒有出現在畫面上的意思是該題有被綁定但父題選取的選項並沒有選到讓該題顯示的選項。
+      // 除了確認該題目是否為必填而且答案是否為空值之外
+      // 如果題目是必填但沒有出現在畫面上則忽略該題
+      // 沒有出現在畫面上的意思是該題有被綁定但父題選取的選項並沒有選到讓該題顯示的選項
       const visibleQuesList = this.form.Questions.filter(item => {
         return document.getElementById(item.Guid);
       });
@@ -145,10 +150,6 @@ export default Vue.component('RenderForm', {
       return !visibleQuesList.some(
         item => item.Required && item.Answer.length === 0
       );
-
-      // return !this.form.Questions.some(
-      //   item => item.Required && item.Answer.length === 0
-      // );
     },
     checkEmail() {
       const hello = this.form.Questions.filter(item => {
@@ -209,5 +210,100 @@ export default Vue.component('RenderForm', {
     openPreviewMode() {
       this.previewMode = true;
     },
+  },
+  created() {
+    this.renderForm({
+      Guid: '10E1F831-12A9-5071-5974-CF39B36C2897',
+      Title: '無標題表單',
+      Description: '我是說明啦乾',
+      Questions: [
+        {
+          Guid: '932B5B39-7C2D-762D-A129-AC6F539A9255',
+          Title: '單選題',
+          Type: 'radio',
+          Options: [
+            {
+              Guid: 'F1FEEDC1-636B-234A-DEC9-B73191D1E899',
+              Value: '選項 1',
+              Binding: [],
+              Score: 0,
+            },
+          ],
+          Required: false,
+          Answer: [],
+        },
+        {
+          Guid: 'F2A67B08-FD82-D5AC-838F-BB020CBA4C9E',
+          Type: 'checkbox',
+          Title: '複選題',
+          Options: [
+            {
+              Guid: '95942DFA-12CA-ECC5-5ED1-2C54A3238879',
+              Value: '選項 1',
+              Binding: [],
+              Score: 69,
+            },
+          ],
+          Required: false,
+          Answer: [],
+        },
+        {
+          Guid: 'F1CBC1BE-409B-0CBC-3F6A-5F905A778C06',
+          Type: 'dropdown',
+          Title: '下拉選單',
+          Options: [
+            {
+              Guid: '687E2475-9662-74ED-5F4E-6C72B5443A13',
+              Value: '選項 1',
+              Binding: [],
+              Score: 0,
+            },
+          ],
+          Required: false,
+          Answer: [],
+        },
+        {
+          Guid: 'B3168FC1-CE21-D252-D619-53BC1356568D',
+          Type: 'literal',
+          Title: '簡答',
+          Options: [],
+          Required: false,
+          Answer: [],
+        },
+        {
+          Guid: '0D323ECC-3FF4-92F3-E72E-6B8D09D12BA0',
+          Type: 'date',
+          Title: '日期',
+          Options: [],
+          Required: false,
+          Answer: [],
+        },
+        {
+          Guid: '9F4E6DA0-9C7A-0DE2-6533-3ABCA5152D4B',
+          Type: 'number',
+          Title: '數字',
+          Options: [],
+          Required: false,
+          Answer: [],
+        },
+        {
+          Guid: 'CE47293C-8D50-6AA4-05CB-93145FECE212',
+          Type: 'email',
+          Title: '信箱',
+          Options: [],
+          Required: false,
+          Answer: [],
+        },
+        {
+          Guid: '85EE85B8-C4DB-87FB-0341-A3B2CBD45635',
+          Type: 'english',
+          Title: '英文',
+          Options: [],
+          Required: false,
+          Answer: [],
+        },
+      ],
+      ScoreEnable: true,
+    });
   },
 });
